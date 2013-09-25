a0d1e178cf84e6ed07fb2cda2a3137096b9e9473
chartly <chartly@outlook.com>
2013-09-25 15:52:52 -0700

    Now runs premake4-dit.lua as the primary build script.

--- build/GenerateBuild.bat
+++ build/GenerateBuild.bat
@@ -2,7 +2,7 @@
 goto menu
 
 :menu
-echo Build Project Generator:
+echo DigiPen Assignment Project Generator:
 echo.
 echo [0] Clean
 echo [1] Visual C++ 2012
@@ -18,12 +18,12 @@ if "%C%"=="1" goto vs2012
 if "%C%"=="0" goto clean
 
 :clean
-"../tools/premake4" --file=premake/premake4-cs350.lua clean
+"../tools/premake4" --file=premake/premake4-dit.lua clean
 "../tools/premake4" --file=premake/premake4-deps.lua clean
 goto quit
 
 :vs2012
-"../tools/premake4" --file=premake/premake4-cs350.lua vs2012
+"../tools/premake4" --file=premake/premake4-dit.lua vs2012
 "../tools/premake4" --file=premake/premake4-deps.lua vs2012
 goto quit
 
